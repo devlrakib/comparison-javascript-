3939a7ae4b704575260541d255183b2d0b8a745d
devlrakib <devrakib5@gmail.com>
2024-03-14 15:05:36 +0600

    first commit

---
+++ comparison.js
@@ -0,0 +1,9 @@
+
+var fruits = ['Apple', 'Banana', 'Orange'];
+fruits[1] = "Mango";
+console.log(fruits);
+
+var fruits = ['Apple', 'Banana', 'Orange'];
+fruits[2] = "Watermelon";
+console.log(fruits);
+
